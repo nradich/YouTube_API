4d55b1723a0da0f0cdb422b4acdd9a483906ce6d
avanickradich <nicholasradich1@gmail.com>
2024-05-16 14:26:35 -0700

    common commands to ineract with ADLS from Databricks

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
 #first commit 
+
+
+#commmom commands for interacting with ADLS from DataBricks 
+
+#for access key 
+spark.conf.set(
+    "fs.azure.account.key.<storage-account>.dfs.core.windows.net",
+    dbutils.secrets.get(scope="<scope>", key="<storage-account-access-key>"))
+
+#TO Display secret scope
+dbutils.secrets.listScopes()
+#To display list of secrets
+dbutils.secrets.list(scope = '<Scope Name>')
+#For list of all command
+dbutils.secrets.help()
